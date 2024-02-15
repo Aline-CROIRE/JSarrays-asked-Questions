e5894e0a5bc9e2afdd55c66639a3d2e2e9275f2b
Aline NIYONIZERA <nikyocroirealine@gmail.com>
2024-02-15 00:19:18 -0800

    This contains javascript arrays basic operations

---
+++ basicoperations.js
@@ -0,0 +1,10 @@
+// Creating array
+let colors=['blue','red','green'];
+//add element at the end 
+colors.push('yellow');
+//Removing last element
+colors.pop();
+//access second element
+let secondelement= colors[1];
+//check if element is included
+let isIncuded=colors.includes('green');
